988ecde94a30b4c6f361ece5cb61719aaaf6bad0
Imasha Senarath <imashadsenarath@gmail.com>
2020-12-26 14:28:35 +0530

    complete first stage

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import Navbar from "./components/navbar.component"
 import ExercisesList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
-import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
        <Route path="/" exact component={ExercisesList} />
        <Route path="/edit/:id" component={EditExercise} />
        <Route path="/create" component={CreateExercise} />
-       <Route path="/user" component={CreateUser} />
       </div>
     </Router>
   );
